9495bde1002e363e83dee41449300c630263e7b2
leviaraujo182 <31797494+leviaraujo182@users.noreply.github.com>
2021-06-28 20:30:56 -0300

    Update

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import './List.css'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser, faBook, faLink } from '@fortawesome/free-solid-svg-icons'
+
 
 function List(){
     return (
         <div className="container">
-            <ul>
-                <li>😻 author:</li>
-                <li>title:</li>
-                <li>url:</li>
-            </ul>
+            <dl>
+                <dt className="item_list"><FontAwesomeIcon icon={faUser}/> Author: </dt>
+                <dt className="item_list"><FontAwesomeIcon icon={faBook}/> Title: </dt>
+                <dt className="item_list"><FontAwesomeIcon icon={faLink}/> Url: </dt>
+            </dl>
+
         </div>
     )
 }
